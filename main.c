b5c4577559341e298136601b789b8d1127df5f05
TazSaz <tazs0166@gmail.com>
2024-01-18 19:56:32 -0500

    Started working with ncurses, initial commit to test functionality on homer

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include "sheet.h"
+#include <ncurses.h>
 
 int main(int argc, char **argv) {
-    int* dims = prompt();
-    struct sheet s;
-    memcpy(s.dim, dims, sizeof(dims));
-    initialize(s);
-    display(s);
+    initscr(); //Initialize screen: clear screen and set up memory
+
+
+
+    refresh(); //Refresh screen to match what is in memory
+
+    getch(); //Wait for user input (keypress) and returns INT value of key
+
+
+    endwin(); //Deallocate memory and end ncurses
     return 0;
 }   
